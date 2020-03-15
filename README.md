9e905b74bedaea2fa33fd0463294bd4700ba48f4
Khushraj Rathod <khushraj.rathod@gmail.com>
2020-03-15 12:22:53 +0530

    First Working Version

---
+++ README.md
@@ -0,0 +1,20 @@
+# TeleDrive 
+
+Uses TDlib and Airgram to create a Google Backup and Sync style uploader.
+
+## Installation
+1. Clone repository:
+```bash
+$ git clone https://github.com/KhushrajRathod/TeleDrive
+```
+2. Install dependencies:
+
+```bash
+$ cd TeleDrive
+$ yarn
+```
+
+3. Run:
+```bash
+yarn start
+```
---
+++ src/signin/renderer.js
@@ -0,0 +1,84 @@
+const { ipcRenderer } = require('electron')
+
+window.addEventListener('DOMContentLoaded', () => {
+    const title = document.getElementById('title')
+    const name = document.getElementById('name')
+    const number = document.getElementById('number')
+    const description = document.getElementById('description')
+    const button = document.getElementById('next')
+    const profile = document.getElementById('profile')
+    const input = document.getElementById('input')
+
+    ipcRenderer.on('auth', async (event, message) => {
+        console.log(message)
+        if (message === 'phoneNumber') {
+            ensureVisible()
+            ipcRenderer.send('phoneNumber', await getInput())
+        } else if (message === 'authCode') {
+            ensureVisible()
+            description.innerHTML = 'Please enter OTP'
+            input.placeholder = 'One time password'
+            ipcRenderer.send('authCode', await getInput())
+        } else if (message === 'password') {
+            ensureVisible()
+            description.innerHTML = 'Please enter your 2FA Password'
+            input.placeholder = '2 Factor Auth Password'
+            input.type = 'password'
+            ipcRenderer.send('password', await getInput())
+        }
+
+        function ensureVisible () {
+            description.style.display = '' // Setting display to '' resets display to initial state
+            button.style.display = ''
+            input.style.display = ''
+            input.type = ''
+            title.innerHTML = 'Sign in to TeleDrive'
+        }
+
+        function getInput () {
+            function modifyUI () {
+                let value = input.value
+                input.value = ''
+                title.innerHTML = 'Working...'
+                description.innerHTML = ''
+                return value
+            }
+
+            return new Promise(resolve => {
+                function clicked () {
+                    button.removeEventListener('click', clicked)
+                    input.removeEventListener('keydown', pressed)
+                    resolve(modifyUI())
+                }
+
+                function pressed (event) {
+                    if (event.keyCode === 13) {
+                        button.removeEventListener('click', clicked)
+                        input.removeEventListener('keydown', pressed)
+                        resolve(modifyUI())
+                    }
+                }
+
+                button.addEventListener('click', clicked)
+                input.addEventListener('keydown', pressed)
+            })
+        }
+    })
+
+    ipcRenderer.on('authSuccess', (event, myInfo) => {
+        title.innerHTML = 'Login Successful'
+        profile.style.display = ''
+        name.innerHTML = myInfo.name
+        number.innerHTML = myInfo.number
+
+        description.innerHTML = 'Select the location for <br> your synced folder'
+        description.style.display = ''
+        button.innerHTML = 'Open'
+        button.style.display = ''
+        input.style.display='none'
+
+        console.log(myInfo)
+        console.log('[IMG]')
+        console.log(myInfo.photo)
+    })
+})
