cf3539642f6fcc6261203d82352ed5c9205748aa
Khushraj Rathod <khushraj.rathod@gmail.com>
2020-04-19 00:32:21 +0530

    Work in progress

--- src/telegram-binder/index.js
+++ src/telegram-binder/index.js
@@ -1,6 +1,8 @@
-const { ipcMain } = require('electron')
-const { Airgram, toObject } = require('airgram')
-const { join } = require('path')
+const {ipcMain, dialog} = require('electron')
+const {Airgram, toObject} = require('airgram')
+const {join} = require('path')
+const Store = require('electron-store')
+const store = new Store()
 
 /**
  * @param {Airgram} client
@@ -12,29 +14,25 @@ module.exports.authenticate = async (client, mainWindow) => {
     await client
 
     client.use(async (ctx, next) => {
-        if ( ctx._ === 'updateAuthorizationState') {
+        if (ctx._ === 'updateAuthorizationState') {
             console.log(`[authState][${ctx._}]`, JSON.stringify(ctx.update))
 
             if (ctx.update.authorizationState._ === "authorizationStateWaitPhoneNumber") {
                 await client.api.setAuthenticationPhoneNumber({
                     phoneNumber: await get('phoneNumber', false),
                     settings: {
-                        allowFlashCall: false
+                        allowFlashCall: false,
+                        isCurrentPhoneNumber: false,
+                        allowSmsRetrieverApi: false
                     }
                 })
             } else if (ctx.update.authorizationState._ === "authorizationStateWaitCode") {
-                await client.api.setAuthenticationPhoneNumber({
-                    phoneNumber: await get('code', false),
-                    settings: {
-                        allowFlashCall: false
-                    }
+                await client.api.checkAuthenticationCode({
+                    code: await get('authCode', false),
                 })
             } else if (ctx.update.authorizationState._ === "authorizationStateWaitPassword") {
-                await client.api.setAuthenticationPhoneNumber({
-                    phoneNumber: await get('phoneNumber', false),
-                    settings: {
-                        allowFlashCall: false
-                    }
+                await client.api.checkAuthenticationPassword({
+                    password: await get('password', false),
                 })
             }
         }
@@ -65,6 +63,8 @@ module.exports.authenticate = async (client, mainWindow) => {
  * @param {string} appPath
  * */
 module.exports.create = (appStorage, appPath) => {
+    console.log("App storage:")
+    console.log(appStorage)
     // noinspection JSCheckFunctionSignatures
     return new Airgram({
         apiId: '1013617',
@@ -82,39 +82,67 @@ module.exports.create = (appStorage, appPath) => {
  * @param {BrowserWindow} mainWindow
  * */
 module.exports.updateInfo = async (client, mainWindow) => {
-    console.log("Waiting for auth...")
-    let me = toObject(await client.api.getMe())
+    const update = async () => {
+        let me = toObject(await (await client).api.getMe())
 
-    // Fetch personal chat ASAP
-    await client.api.getChats({
-        limit: 1, // limit: 1 still fetches all chats from the server
-        offsetChatId: 0,
-        offsetOrder: '9223372036854775807'
-    })
-
-    // To increase speed, first send all info, then check if photo is download, if not, download, then update again
-    let myInfo = {
-        name: me.lastName ? me.firstName + ' ' + me.lastName : me.firstName,
-        number: '+' + me.phoneNumber,
-        photo: me.profilePhoto.small.local.path
-    };
-
-    (await mainWindow).webContents.send('updateMyInfo', myInfo)
-
-    // If photo not already downloaded
-    if (!myInfo.photo) {
-        await client.api.downloadFile({
-            fileId: me.profilePhoto.small.id,
-            priority: 32
+        // Fetch personal chat ASAP
+        await client.api.getChats({
+            limit: 1, // limit: 1 still fetches all chats from the server
+            offsetChatId: 0,
+            offsetOrder: '9223372036854775807'
         })
-        // Listen for completion of photo download
-        client.use(async (ctx, next) => {
-            if (ctx.update.file.remote.id === me.profilePhoto.small.remote.id && ctx.update.file.local.isDownloadingCompleted) {
-                // Set photo and update again
-                myInfo.photo = ctx.update.file.local.path;
-                (await mainWindow).webContents.send('updateMyInfo', myInfo)
+
+        // To increase speed, first send all info, then check if photo is download, if not, download, then update again
+        let myInfo = {
+            name: me.lastName ? me.firstName + ' ' + me.lastName : me.firstName,
+            number: '+' + me.phoneNumber,
+            photo: me.profilePhoto.small.local.path
+        };
+
+        (await mainWindow).webContents.send('authSuccess');
+        (await mainWindow).webContents.send('updateMyInfo', myInfo)
+
+        // If photo not already downloaded
+        if (!myInfo.photo) {
+            await client.api.downloadFile({
+                fileId: me.profilePhoto.small.id,
+                priority: 32
+            })
+            // Listen for completion of photo download
+            client.use(async (ctx, next) => {
+                if (ctx._ === "updateFile" && ctx.update.file.remote.id === me.profilePhoto.small.remote.id && ctx.update.file.local.isDownloadingCompleted) {
+                    // Set photo and update again
+                    myInfo.photo = ctx.update.file.local.path;
+                    (await mainWindow).webContents.send('updateMyInfo', myInfo)
+                }
+                return next()
+            })
+        }
+
+        let stored = store.get('teleDir')
+        if (stored) {
+            (await mainWindow).webContents.send('selectedDir', stored)
+        }
+
+        ipcMain.on('openFileDialog', async () => {
+            let response = await dialog.showOpenDialog({properties: ['openDirectory']})
+            if (!response.canceled) {
+                let teleDir = join(response.filePaths[0], 'TeleDriveSync');
+                try {
+                    store.set('teleDir', teleDir)
+                } catch (e) {
+                    console.error(e)
+                }
+                (await mainWindow).webContents.send('selectedDir', teleDir)
+            }
+        })
+    }
+    if ((await client.api.getAuthorizationState()).response._ !== "authorizationStateReady") {
+        client.use(async (response, next) => {
+            if (response._ === 'updateAuthorizationState' && response.update.authorizationState._ === "authorizationStateReady") {
+                await update();
             }
             return next()
         })
-    }
+    } else await update()
 }
