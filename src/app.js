4f8b59800308b829cad62c8dc69ca2c31f32a142
Khushraj Rathod <khushraj.rathod@gmail.com>
2020-07-23 16:25:09 +0530

    Get rid of squirrel stuff

--- src/app.js
+++ src/app.js
@@ -2,11 +2,6 @@ const {app, BrowserWindow, shell, ipcMain, dialog} = require('electron');
 const path = require('path');
 const {authenticate, create, updateInfo} = require(path.join(__dirname, 'telegram-binder', 'index.js'))
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-    app.quit();
-}
-
 let mainWindow
 const createWindow = async () => {
     mainWindow = new Promise(async resolve => {
