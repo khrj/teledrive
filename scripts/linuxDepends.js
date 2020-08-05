67eae5565fe60cee956f7d8a705fb84bc842ff8a
Khushraj Rathod <khushraj.rathod@gmail.com>
2020-08-05 08:36:29 +0530

    Add dependencies for AppImage
    Test
    
    
    Test

---
+++ scripts/linuxDepends.js
@@ -0,0 +1,8 @@
+exports.default = async context => {
+    if (context.electronPlatformName === "linux") {
+        const { copyFile } = require("fs").promises
+        const { join } = require("path")
+        await copyFile("/usr/lib/x86_64-linux-gnu/libc++.so.1", join(context.appOutDir, "libc++.so.1"))
+        await copyFile("/usr/lib/x86_64-linux-gnu/libssl.so.1.0.0", join(context.appOutDir, "libssl.so.1.0.0"))
+    }
+}
