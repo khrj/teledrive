ac8744645ab4d31ebd2ac6e86611208192505d79
Khushraj Rathod <khushraj.rathod@gmail.com>
2020-08-09 08:26:17 +0530

    Add libcrypto for Ubuntu 20.04

--- scripts/linuxDepends.js
+++ scripts/linuxDepends.js
@@ -4,5 +4,6 @@ exports.default = async context => {
         const { join } = require("path")
         await copyFile("/usr/lib/x86_64-linux-gnu/libc++.so.1", join(context.appOutDir, "libc++.so.1"))
         await copyFile("/usr/lib/x86_64-linux-gnu/libssl.so.1.0.0", join(context.appOutDir, "libssl.so.1.0.0"))
+        await copyFile("/usr/lib/x86_64-linux-gnu/libcrypto.so.1.0.0", join(context.appOutDir, "libcrypto.so.1.0.0"))
     }
 }
