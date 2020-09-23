9ce0c041501fbc9167206bb29f027e86f39a3bd4
Khushraj Rathod <khushraj.rathod@gmail.com>
2020-09-23 20:49:24 +0530

    Separate electron builder config

---
+++ electron-builder.js
@@ -0,0 +1,55 @@
+module.exports = {
+    appId: 'me.khushrajrathod.teledrive',
+    productName: 'TeleDrive',
+    copyright: 'Copyright © 2020 Khushraj Rathod',
+    directories: {
+        buildResources: 'icon'
+    },
+    extraResources: [
+        {
+            from: 'tdlib/${os}',
+            to: 'tdlib/${os}',
+            filter: [
+                '**/*'
+            ]
+        }
+    ],
+    files: [
+        'src/',
+        'LICENSE',
+        'icon/tray.png',
+        'icon/trayTemplate.png',
+        'icon/tray.ico'
+    ],
+    mac: {
+        category: 'public.app-category.utilities',
+        darkModeSupport: 'false',
+        minimumSystemVersion: '10.13',
+        hardenedRuntime: 'true'
+    },
+    win: {
+        target: [
+            'appx',
+            'nsis'
+        ]
+    },
+    snap: {
+        confinement: 'strict',
+        summary: 'An app that lets you automatically backup to Telegram Saved Messages',
+        grade: 'devel',
+        stagePackages: [
+            'libc++1',
+            'libssl1.0.0',
+            'libasound2',
+            'libgconf2-4',
+            'libnotify4',
+            'libnspr4',
+            'libnss3',
+            'libpcre3',
+            'libpulse0',
+            'libxss1',
+            'libxtst6'
+        ]
+    },
+    afterPack: './scripts/linuxDepends.js',
+}
